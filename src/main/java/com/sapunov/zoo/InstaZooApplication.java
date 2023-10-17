8a106dec0d8444070dc0cfba7e76a55f5e944ab0
Alexey <blacka8607@rambler.ru>
2023-10-17 12:45:42 +0300

    Initial commit

---
+++ src/main/java/com/sapunov/zoo/InstaZooApplication.java
@@ -0,0 +1,13 @@
+package com.sapunov.zoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaZooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaZooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapunov/zoo/InstaZooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapunov.zoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaZooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
